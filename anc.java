526494d5417f30836d6b92467f4dff614c9c53dd
anuj <anujjain.cse22@jecrc.ac.in>
2020-07-09 23:15:47 +0530

    dev change

--- anc.java
+++ anc.java
@@ -4,7 +4,7 @@ public class anc
     public static void main(String [] args)
     {
         System.out.println("abc");
-        System.out.println("abc");
+        System.out.println("in dev branch");
     }
 }
 
